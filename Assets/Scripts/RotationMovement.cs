eb9ef82908779feaad28a23b36f895ebb41198ea
Thomas Bispo Santos <tomasbs2025@hotmail.com>
2019-09-27 10:53:35 -0300

    Assets

---
+++ Assets/Scripts/RotationMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RotationMovement : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    void Start()
+    {
+        if (target == null) {
+            target = GetComponent<Transform>();
+        }
+    }
+
+    void Update()
+    {
+        transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
